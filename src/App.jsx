993c294e651ce0a5b78804e715a8aac37fb2e853
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-08-27 22:02:07 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>You're welcome to JobWeave</h1>
+      <br />
+      <h2>The Modern Platform Help To Map Your Skills to Your Dream Career</h2>
+      <br />
+      <br />
+      <p>
+        Discover the fastest, most achievable path from the skills you have
+        today to the job you want tomorrow. Built on the comprehensive Job Weave
+        taxonomy.
+      </p>
+    </>
+  );
+}
+
+export default App;
